3378beefcce6eaf565142bd882c0032fc6c08775
rhogotargaryen <dhaller.personal@gmail.com>
2019-01-28 01:34:17 -0800

    postgres's for heroku deploment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181209083805) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "img_url"
